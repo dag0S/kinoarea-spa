7d1e2cbd44b4e789c435c10d05feeaf16bf40a1f
Gosudarev Danila <danidagosudarev@gmail.com>
2024-02-20 23:44:39 +0300

    added paralax

--- src/layouts/AuthLayout/AuthLayout.tsx
+++ src/layouts/AuthLayout/AuthLayout.tsx
@@ -1,19 +1,21 @@
 import { Outlet } from 'react-router-dom';
 import Logo from '../../components/Logo/Logo';
 import { FC } from 'react';
+import { MouseParallaxContainer, MouseParallaxChild } from 'react-parallax-mouse';
 
 import styles from './AuthLayout.module.scss';
 
 const AuthLayout: FC = () => {
   return (
-    <div className={styles['layout']}>
+    <MouseParallaxContainer className={styles['layout']}>
       <div className={styles['layout__left']}>
+        <MouseParallaxChild factorX={0.005} factorY={0.005} className={styles['layout__img']} />
         <Logo className={styles['layout__logo']} />
       </div>
       <div className={styles['layout__right']}>
         <Outlet />
       </div>
-    </div>
+    </MouseParallaxContainer>
   );
 };
 
